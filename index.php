75d9c2dd6cc144b3a81b3be95b506d998be28417
Sylvain NGO <ngo.sylvain@gmail.com>
2018-07-13 15:15:43 +0200

    Accueil

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ require_once __DIR__ . '/include/init.php';
 
 require __DIR__ . '/layout/top.php';
 ?>
-<h1>Accueil</h1>   
+<h1>Page Accueil</h1>   
+<p>welcome</p>
+<p> sur la boutique</p>
 <?php
 require __DIR__ . '/layout/bottom.php';
 ?>
